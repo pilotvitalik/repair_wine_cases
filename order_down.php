79611abb426c83766f29437594e1ccc6fe213f75
pilotvitalik <itvkip@yandex.ru>
2019-08-01 13:39:27 +0300

    Добавлена обработка форм вызова мастера(вверху и внизу страницы)

---
+++ order_down.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$maker = $_POST['maker'];
+	$message = $_POST['message'];
+
+	$error = '';
+
+	if(trim($name) == ''){
+		$error = 'Укажите имя';
+	}else if(trim($phone) == ''){
+		$error = 'Укажите телефон';
+	}else if(trim($maker) == ''){
+		$error = 'Укажите производителя';
+	}else if(strlen($message) < 5){
+		$error = 'Напишите сообщение длиной больше 5 букв';
+	}
+	
+	if($error !=''){
+		echo $error;
+		exit;
+	}
+
+	$subject = "=?utf-8?B?".base64_encode('Тестовое сообщение')."?=";
+    $message1 .='Name:'.$name."\r\n";
+    $message1 .='Phone:'.$phone."\r\n";
+    $message1 .='Maker:'.$maker."\r\n";
+    $message1 .='Сообщение:'.$message."\r\n";
+
+
+	mail('itvkip@yandex.ru',$subject,$message1);
+
+	header('Location: /index.html');
+?>
